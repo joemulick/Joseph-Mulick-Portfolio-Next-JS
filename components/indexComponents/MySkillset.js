983ecb138e1e53d3e6a42604307f5f50ecfd20a6
Joe Mulick <joemulick@gmail.com>
2017-11-09 20:37:34 -0800

    starting on my projects section

--- components/indexComponents/MySkillset.js
+++ components/indexComponents/MySkillset.js
@@ -5,24 +5,41 @@ import JAVASCRIPT from './svgIcons/JAVASCRIPT.js'
 import JQUERY from './svgIcons/JQUERY.js'
 import BOOTSTRAP from './svgIcons/BOOTSTRAP.js'
 import GIT from './svgIcons/GIT.js'
+import NODE from './svgIcons/NODE.js'
+import MYSQL from './svgIcons/MYSQL.js'
+import MONGODB from './svgIcons/MONGODB.js'
+import EXPRESS from './svgIcons/EXPRESS.js'
+import REACT from './svgIcons/REACT.js'
+import AWS from './svgIcons/AWS.js'
 
 
 const MySkillset = (props) => (
 
 
 <Row>
-
 	<Col>
-
-		<HTML />
-		<CSS />
-		<JAVASCRIPT />
-		<JQUERY />
-		<BOOTSTRAP />
-		<GIT />
-
+		<div>
+			<HTML />
+			<CSS />
+			<JAVASCRIPT />
+			<JQUERY />
+			<BOOTSTRAP />
+			<GIT />
+		</div>
+	</Col>
+	<Col>
+		<div>
+			<NODE />
+			<MYSQL />
+			<MONGODB />
+			<EXPRESS />
+			<REACT />
+			<AWS />
+		</div>
     </Col>
+     <style jsx>{`
 
+                `}</style>
 </Row>
 
 )
--- out/_next/fa448d48-f101-4a13-925c-c7c9abdf577c/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([6],{192:function(e,t,u){e.exports=u(193)},193:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(0),a=n(l),o=u(70),r=n(o);t.default=function(){return a.default.createElement(r.default,null,a.default.createElement("p",null,"This is the about page"))}}},[192]);
-            return { page: comp.default }
-          })
-        
