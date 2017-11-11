4d869efb3d17a3c725f964d33192fb339478b894
Joe Mulick <joemulick@gmail.com>
2017-11-10 17:39:06 -0800

    altered intro section so the spacing is better and took out some css that is not being used

--- components/indexComponents/IntroTop.js
+++ components/indexComponents/IntroTop.js
@@ -5,17 +5,21 @@ const IntroTop = (props) => (
  <Row>
     <Col>
 
-      <div className="intro-top-container">
+      <div className="intro-top-outer-container">
 
-        <div className="text-div-container">
-          <h1 className="nameLine">My name is Joe Mulick</h1>
-          <h2 className="nameLine">Full Stack Javascript Web Developer</h2> 
-          <h2 className="nameLine">Los Angeles California</h2>
-        </div>
+        <div className="intro-textandphoto-container">
 
-        <div className="me-photo-div">
-          <img className="me-photo" src="https://res.cloudinary.com/ddsihrmda/image/upload/v1510035228/mefiltered_qff7c0.jpg" />
-        </div>   
+          <div className="text-div-container">
+            <h1 className="nameLine">My name is Joe Mulick</h1>
+            <h2 className="nameLine">Full Stack Javascript Web Developer</h2> 
+            <h2 className="nameLine">Los Angeles California</h2>
+          </div>
+
+          <div className="me-photo-div">
+            <img className="me-photo" src="https://res.cloudinary.com/ddsihrmda/image/upload/v1510035228/mefiltered_qff7c0.jpg" />
+          </div>
+
+        </div> 
 
       </div>
 
@@ -31,32 +35,31 @@ const IntroTop = (props) => (
     <Col>
       <img className="los-angeles" height="130px" width="auto" src="https://res.cloudinary.com/ddsihrmda/image/upload/v1510361407/LAsmlgreyone_oswasr.png" alt="photo-of-los-angeles" />
     </Col>
-    
+
   </Row>
                         <style jsx>{`
 
                       .nameLine {
                         text-align: center;
                       }
-                      .text-div-container {
-                        margin: auto;
-                        width: 75%;
-                      }
                       .me-photo{
-                        max-width: 40%;
-                        margin-left: 10%;
-                        height: auto;
+                        max-height: 140px;           
+                        width: auto;
+                        margin-left: 20px;
                         border-radius: 50%;
                       }
-                      .me-photo-div{
-                        width: auto;
+                      .text-div-container{
+                        display: inline-block;
+                        flex: 0 0 65%;
+
                       }
-                      .intro-top-container{
-                        max-width: 85%;
-                        display:flex;
-                        flex-direction:row;
-                        justify-content: space-around;
-                        margin: 45px auto;
+                      .me-photo-div{
+                        display: inline-block;
+                        flex: 1;
+                      }
+                      .intro-textandphoto-container{
+                        display: flex;
+                        margin: auto;
                       }
                       
                   `}</style>
