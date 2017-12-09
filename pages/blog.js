a7ce020186f4842e91d84d933ec0afa06f88786d
Joe Mulick <joemulick@gmail.com>
2017-12-08 21:49:59 -0800

    started updating the blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,11 @@
 import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+import { Row, Col } from 'reactstrap'
 
 export default () => (
-    <Layout>
-       <p>This is the about page</p>
-    </Layout>
+	<div>
+    	<Layout>
+       		<p>This is the about page</p>
+    	</Layout>
+    </div>
 )
